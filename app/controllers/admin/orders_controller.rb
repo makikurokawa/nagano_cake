4e366a3527d38fe795fbd08473bba7f76dea7c24
makikurokawa <110467242+makikurokawa@users.noreply.github.com>
2022-11-10 13:21:35 +0000

    enum修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -8,7 +8,7 @@ class Admin::OrdersController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-    order.update
+    order.update(order_params)
     redirect_to admin_order_path(order.id)
   end
 
